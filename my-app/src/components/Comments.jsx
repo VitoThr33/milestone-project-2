560845d4ae024302b6f0dafa34a490075a607587
Ronaldob8 <97202631+Ronaldob8@users.noreply.github.com>
2022-10-19 19:46:18 -0700

    added a Cards and Comments component

---
+++ my-app/src/components/Comments.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {CardContent, Card, Typography} from '@mui/material'
+
+function Comments() {
+    return(
+        <Card>
+            <CardContent>
+                <Typography variant='body2'>
+                    this is the details content
+                </Typography>
+            </CardContent>
+            
+        </Card>
+    )
+}
+
+
+
+export default Comments
