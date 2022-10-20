58d61d01d9daa4c8f3471299d0a6fa1532e1a94f
Ronaldob8 <97202631+Ronaldob8@users.noreply.github.com>
2022-10-19 22:36:09 -0700

    added multiline to description

--- my-app/src/components/Comments.jsx
+++ my-app/src/components/Comments.jsx
@@ -5,8 +5,30 @@ function Comments() {
     return(
         <Card>
             <CardContent>
-                <Typography variant='body2'>
-                    this is the details content
+                <Typography 
+                textAlign={'left'}
+                variant='h6'
+                border={2}
+                borderColor='black'
+                >
+                    Comment user name
+                </Typography>
+                <Typography
+                border={2}
+                borderColor='black' 
+                textAlign={'left'}
+                sx={{maxWidth: 600, minWidth: 600}}
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Quis viverra nibh cras pulvinar mattis nunc sed blandit libero. 
+                    Phasellus vestibulum lorem sed risus ultricies tristique nulla 
+                    aliquet enim. Tempus quam pellentesque nec nam aliquam. 
+                    Vel risus commodo viverra maecenas accumsan lacus vel facilisis. 
+                    Nulla aliquet enim tortor at. Volutpat consequat mauris nunc congue 
+                    nisi vitae suscipit tellus mauris. Ultrices in iaculis nunc sed 
+                    augue lacus viverra vitae congue. Vitae auctor eu augue ut. 
+                    Amet dictum sit amet justo donec enim diam vulputate.
                 </Typography>
             </CardContent>
             
