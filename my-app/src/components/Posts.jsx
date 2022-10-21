f753e2763f981f78f90f6141ab3ab62cc17443ed
Menegoth <antonioareas@me.com>
2022-10-20 21:54:15 -0700

    added cors protection

--- my-app/src/components/Posts.jsx
+++ my-app/src/components/Posts.jsx
@@ -1,22 +1,7 @@
 import React from 'react'
 import Cards from './Cards'
-import axios from "axios"
 
 function Post() {
-
-  // const response = await fetch("https://cspn-sports.herokuapp.com/posts");
-  // const result = await response.json();
-  // console.log(result);
-
-  axios({
-    method: "get",
-    url: "http://cspn-sports.herokuapp.com/posts"
-  })
-  .then(res => {
-    res.json();
-  }).then(result => {
-    console.log(result);
-  })
 
   return (
     <div>
