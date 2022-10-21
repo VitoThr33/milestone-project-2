b0bfa88ee5d685e6acab18cd1f099889602d483f
Menegoth <antonioareas@me.com>
2022-10-20 20:30:24 -0700

    cors

--- my-app/src/components/Posts.jsx
+++ my-app/src/components/Posts.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import Cards from './Cards'
+
 function Post() {
+
+  // const response = await fetch("https://cspn-sports.herokuapp.com/posts");
+  // const result = await response.json();
+  // console.log(result);
+
+  fetch("https://cspn-sports.herokuapp.com/posts")
+  .then(res => {
+    res.json();
+  }).then(result => {
+    console.log(result);
+  })
+
   return (
     <div>
       <Cards />
--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const cors = require("cors");
 
 //configuration
-app.use(cors());
 require("dotenv").config({path: ".env"});
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 //GET /
