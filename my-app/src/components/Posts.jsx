786931caede2d0f3abce3a7292e1911f278ca8d7
Menegoth <antonioareas@me.com>
2022-10-24 12:51:41 -0700

    added title to models and test fetching

--- my-app/src/components/Posts.jsx
+++ my-app/src/components/Posts.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import Cards from './Cards'
 import Navbar from './Navbar'
+import {useEffect, useState, useRef} from "react";
 
 function Post() {
+
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://cspn-sports.herokuapp.com/posts/");
+      const result = await response.json();
+      setData(result);
+    }
+    getData();
+
+  }, [])
+
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log(data);
+      setIsLoading(false);
+    } else {
+      isMounted.current = true;
+    }
+  }, [data])
+
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
 
   return (
     <div>
